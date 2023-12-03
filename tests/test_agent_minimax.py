e0e9fefe365724cbbaaf21cf14c0fd9deca97d46
Arina <arishkabelova1999@gmail.com>
2023-12-03 19:22:00 +0000

    Fixed an issue of changing agent roles.

--- tests/test_agent_minimax.py
+++ tests/test_agent_minimax.py
@@ -3,7 +3,7 @@ from agents.agent_minimax.minimax_agent import *
 from game_utils import *
     
 
-def test_agent_move_maximizing():
+def test_the_fact_that_max_agent_moves():
     board = np.array([
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
@@ -23,11 +23,105 @@ def test_agent_move_maximizing():
     action, score = agent_move(board, current_depth, target_depth, is_our_player, player, alpha, beta)
     
     assert action in [0, 1, 2, 3, 4, 5, 6]
-
     assert isinstance(score, int)
 
+ 
+ 
+def test_forced_vertical_win_move():
+    board = np.array([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 2, 0, 0, 0, 0, 0]
+    ])  
 
-def test_agent_move_minimizing():
+    board = board[::-1]
+    player = PLAYER1
+    current_depth = 0
+    target_depth = 2
+    is_our_player = True
+    alpha = -np.inf
+    beta = np.inf
+
+    action, _ = agent_move(board, current_depth, target_depth, is_our_player, player, alpha, beta)
+    
+    assert action == 0
+
+
+def test_forced_horizontal_win_move():
+    board = np.array([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0, 0]
+    ])  
+    board = board[::-1]
+
+    player = PLAYER1
+    current_depth = 0
+    target_depth = 1
+    is_our_player = True
+    alpha = -np.inf
+    beta = np.inf
+
+    action, score = agent_move(board, current_depth, target_depth, is_our_player, player, alpha, beta)
+    
+    assert action == 3   
+    assert score == 10000
+
+
+def test_forced_diagonal_win_move():
+    board = np.array([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 2, 0, 0, 0],
+        [0, 1, 2, 2, 0, 0, 0],
+        [1, 2, 1, 1, 0, 0, 0]
+    ])  
+
+    board = board[::-1]
+    player = PLAYER1
+    current_depth = 0
+    target_depth = 1
+    is_our_player = True
+    alpha = -np.inf
+    beta = np.inf
+
+    action, score = agent_move(board, current_depth, target_depth, is_our_player, player, alpha, beta)
+    
+    assert action == 3   
+    assert score == 10000   
+
+def test_forced_diagonal_win_move_second_player():
+    board = np.array([
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 2, 0, 0, 0],
+        [0, 1, 2, 2, 2, 0, 0],
+        [1, 2, 1, 1, 2, 2, 2]
+    ])  
+
+    board = board[::-1]
+    player = PLAYER2
+    current_depth = 0
+    target_depth = 1
+    is_our_player = False
+    alpha = -np.inf
+    beta = np.inf
+
+    action, score = agent_move(board, current_depth, target_depth, is_our_player, player, alpha, beta)
+    
+    assert action == 2  
+    assert score == -10000     
+
+
+def test_the_fact_that_min_agent_moves():
     board = np.array([
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
@@ -47,7 +141,6 @@ def test_agent_move_minimizing():
     action, score = agent_move(board, current_depth, target_depth, is_our_player, player, alpha, beta)
 
     assert action in [0, 1, 2, 3, 4, 5, 6]
-
     assert isinstance(score, int)
 
 
