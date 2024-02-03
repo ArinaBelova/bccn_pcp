1f667b824c18976e71a2e691f18cf4418f43e438
Arina <arishkabelova1999@gmail.com>
2024-02-03 15:40:16 +0000

    Introducing neural network, buggy code

--- agents/agent_mcts_nn/mcts.py
+++ agents/agent_mcts_nn/mcts.py
@@ -1,6 +1,10 @@
 import random
 from math import *
 import numpy as np
+
+import sys
+sys.path.append('/home/galan/Desktop/programming_project_bccn/')
+
 from game_utils import *
 # from typing import Type
 from agents.agent_minimax import minimax_move
@@ -8,6 +12,9 @@ from agents.agent_minimax import minimax_move
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+
+import datetime
 
 # https://medium.com/oracledevs/lessons-from-implementing-alphazero-7e36e9054191
 # https://www.andrew.cmu.edu/course/10-403/slides/S19_evolutionarymethods.pdf
@@ -27,12 +34,12 @@ class Node:
         self.untriedMoves = get_valid_positions(self.board_state) # future child nodes
         self.playerJustMoved = player # player that just moved is our current player
 
-    def UCT_select_child(self, c = sqrt(2)):
+    def UCT_select_child(self, c = sqrt(0)):
         """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
             lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
             exploration versus exploitation.
         """
-        s = sorted(self.childNodes, key = lambda node: node.wins/node.visits + c * sqrt(log(node.parentNode.visits)/node.visits))[-1]
+        s = sorted(self.childNodes, key = lambda node: node.wins/node.visits + c * sqrt(log(node.parentNode.visits)/node.visits) + 1e-6)[-1]
         return s
     
     def add_child(self, move):
@@ -76,6 +83,28 @@ class Node:
 
         elif winning_player == 3 - our_player:
             return -1    
+
+
+    def get_result_nn(self, value_prediction):
+        """
+        Calculate the rollout result based on the predicted value from the neural network.
+
+        Parameters:
+        - board: The current game board state.
+        - value_prediction: The predicted value from the neural network.
+
+        Returns:
+        - result: The calculated rollout result.
+        """
+        # Example: Convert the value prediction to a result (win/loss/draw)
+        if value_prediction > 0.5:
+            result = 1  # Win
+        elif value_prediction < -0.5:
+            result = -1  # Loss
+        else:
+            result = 0  # Draw
+
+        return result
 
 
     # Update for a particular player
@@ -162,41 +191,41 @@ class MLP(nn.Module):
         return policy_output, value_output
 
 
-# def nn_training_loop(model, dataloader, optimizer, device, is_training):
-#     model.train()
+def nn_training_loop(model, dataloader, optimizer, device, is_training):
+    model.train()
 
-#     # Initialize the total loss for this epoch
-#     epoch_loss = 0  
+    # Initialize the total loss for this epoch
+    epoch_loss = 0  
 
-#     # Loop over the data
-#     for board, policy_value in enumerate(dataloader):
+    # Loop over the data
+    for board, policy_value in enumerate(dataloader):
 
-#         images = images.to(device)
-#         masks = masks.to(device)
+        images = images.to(device)
+        masks = masks.to(device)
 
-#         # Pass the batch through the model
-#         if is_training:
-#             loss = model(images, masks)
-#         else:
-#             with torch.no_grad():
-#                 loss = model(images, masks)
+        # Pass the batch through the model
+        if is_training:
+            loss = model(images, masks)
+        else:
+            with torch.no_grad():
+                loss = model(images, masks)
 
-#         # If in training mode, backpropagate the error and update the weights
-#         if is_training:
-#             loss.backward()
-#             optimizer.step()
-#             optimizer.zero_grad()
+        # If in training mode, backpropagate the error and update the weights
+        if is_training:
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
-#         # update the total loss of the epoch
-#         loss_item = loss.item()
-#         epoch_loss += loss_item
+        # update the total loss of the epoch
+        loss_item = loss.item()
+        epoch_loss += loss_item
 
-#     # Return the average loss for this epoch
-#     return epoch_loss / (batch_id + 1)    
-        # other_player = player
+    # Return the average loss for this epoch
+    return epoch_loss 
 
 
-def UCT(rootstate, player, saved_state, num_iterations, verbose = False):
+
+def UCT(rootstate, player, mlp, num_iterations, verbose = False):
     """ Conduct a UCT search for itermax iterations starting from rootstate.
         Return the best move from the rootstate.
         Assumes 2 alternating players (player 1 starts), with game results in ther ange [0.0, 1.0]."""
@@ -204,7 +233,6 @@ def UCT(rootstate, player, saved_state, num_iterations, verbose = False):
     # TODO: playing from the current player
     rootnode = Node(board_state = rootstate, player = player, move = None, parent = None)
 
-    network = MLP(len(rootstate.flatten()), 7)
     # TODO: all the methods should act on the nodes and not boards/players
     for _ in range(num_iterations):
         node = rootnode
@@ -262,8 +290,12 @@ def UCT(rootstate, player, saved_state, num_iterations, verbose = False):
                 winning_player = 3 - sim_player
                 break
             
+            policy_vector, _ = mlp(copy_board.flatten())
+            move = torch.argmax(policy_vector).item()
+
             # Still need to have it to change the board, check the reference to the player
-            apply_player_action(board = copy_board, action = random.choice(get_valid_positions(copy_board)), player = sim_player)
+            # random.choice(get_valid_positions(copy_board))
+            apply_player_action(board = copy_board, action = move, player = sim_player)
             sim_player = 3 - sim_player
 
             # TODO: There may be a case that there is no connect 4      
@@ -275,12 +307,14 @@ def UCT(rootstate, player, saved_state, num_iterations, verbose = False):
         while node is not None: 
             # results are given the the NN value not this handmade policy
             # TODO: HOW THE FUCK TO DO THIS???
-            result = node.give_results(winning_player = winning_player,our_player=our_player) 
-            # TODO: do a loss here?  nn(board_state, result)
+            #result = node.give_results(winning_player = winning_player, our_player=our_player) 
+            
+            _, value_prediction = mlp(node.board_state.flatten())
+            result = node.give_results_nn(node.board_state, value_prediction)
 
             # result = value # from NN
             node.update(result = result)
-            node = node.parentNode
+            node = node.parentNode # TODO: that's how it was previously! 
 
 
     # Output some information about the tree - can be omitted
@@ -292,5 +326,92 @@ def UCT(rootstate, player, saved_state, num_iterations, verbose = False):
     # return the move that was most visited
     return sorted(rootnode.childNodes, key = lambda node: node.visits)[-1].move # node.wins / node.visits
 
-def mcts_move(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]):
-    return UCT(rootstate = board, player = player, saved_state = saved_state, num_iterations = 5000, verbose = False), saved_state
+def mcts_move(board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState], trained_network: Optional[bool] = False):
+    # TODO: where to create a network so it will train and will not be re-intiialised every time?
+    # SOmehow gve it a trained newotk -> load here saved weights from outside
+    # Don't change the network while playing with human opponent, all the trianing is not the responsbilty of this funciton
+
+    return UCT(rootstate = board, player = player, mlp = mlp, saved_state = saved_state, num_iterations = 5000, verbose = False), saved_state
+
+
+
+
+#=================================== Training the network ================================================
+# Example training data generation
+def generate_training_data(mcts_iterations, mlp, player):
+    training_data = []
+
+    initial_state = np.zeros((7, 6))  # Example initial game state
+    root_state = initial_state.copy()
+    for _ in range(mcts_iterations):
+        # Simulate one MCTS iteration starting from the initial state
+        selected_move = UCT(root_state, player, mcts_iterations, mlp)
+
+        # TODO: problem with board overload
+        # if selected-move is not in get_valid_valid_moves()
+        # break the loop and re-init the root_state
+
+        # Apply the selected move to update the board
+        apply_player_action(board=root_state, action=selected_move, player=player)
+
+        player = 3 - player
+
+        # Get policy and value from the neural network
+        policy_vector, value_prediction = mlp(root_state.flatten())
+
+        # Generate training example
+        training_data.append((root_state.flatten(), policy_vector, value_prediction))
+
+    return training_data
+
+# Example training loop
+def train_neural_network(mlp, optimizer, training_data, num_epochs, batch_size):
+    for epoch in range(num_epochs):
+        random.shuffle(training_data)
+
+        for batch_start in range(0, len(training_data), batch_size):
+            batch = training_data[batch_start:batch_start + batch_size]
+            inputs, targets_policy, targets_value = process_batch(batch)
+
+            optimizer.zero_grad()
+            policy_output, value_output = mlp(inputs)
+
+            loss_policy = mlp.criterion_policy(policy_output, targets_policy)
+            loss_value = mlp.criterion_value(value_output, targets_value)
+
+            total_loss = loss_policy + loss_value
+            total_loss.backward()
+            optimizer.step()
+
+# Example batch processing
+def process_batch(batch):
+    inputs = torch.stack([torch.Tensor(example[0]) for example in batch])
+    targets_policy = torch.stack([torch.argmax(example[1]) for example in batch])
+    targets_value = torch.stack([example[2] for example in batch])
+
+    return inputs, targets_policy, targets_value
+
+
+
+if __name__ == "__main__":
+    # Example usage
+    input_size = 42  # Example input size for a 7x6 Connect4 board
+    num_actions = 7  # Example number of actions (columns to drop pieces into)
+    mlp = MLP(input_size, num_actions)
+    optimizer = optim.Adam(mlp.parameters(), lr=0.001)
+
+    # Example MCTS and training parameters
+    mcts_iterations = 1000
+    num_epochs = 100
+    batch_size = 8 # 32
+
+    # Generate training data using MCTS
+    # TODO: how do we cater for different players? 
+    training_data = generate_training_data(mcts_iterations, mlp, player=1)
+
+    # Train the neural network
+    train_neural_network(mlp, optimizer, training_data, num_epochs, batch_size)
+
+    # save the network
+    name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".pt"
+    torch.save(mlp.state_dict(), f"./models/{name}")
